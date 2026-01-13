697d6bd198b70b71ed307dc39da11012ec7d1520
orangeow <143812812+orangeow@users.noreply.github.com>
2026-01-12 20:52:44 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -63,38 +63,7 @@ bookCover.style.color = textColors[Math.floor(Math.random()*textColors.length)];
 
 };
 
-
-//different structures
-
-// if (randomTitle == 0){
-//     bookCover.innerHTML = "The " + nouns[Math.floor(Math.random()*nouns.length)] + " in the " + nouns[Math.floor(Math.random()*nouns.length)];
-// }
-// if (randomTitle == 1){
-//     bookCover.innerHTML = "The " + adjectives[Math.floor(Math.random()*adjectives.length)] + " " + nouns[Math.floor(Math.random()*nouns.length)];
-// }
-// if (randomTitle == 2){
-//     bookCover.innerHTML = verbs[Math.floor(Math.random()*verbs.length)] + " the " + nouns[Math.floor(Math.random()*nouns.length)];
-// }
-
-// if (randomTitle == 3){
-//     bookCover.innerHTML = "The " + nouns[Math.floor(Math.random()*nouns.length)];
-// }
-
-// if (randomTitle == 4){
-//     bookCover.innerHTML = "The " + nouns[Math.floor(Math.random()*nouns.length)] + " of the " + nouns[Math.floor(Math.random()*nouns.length)];
-// }
-
-// if (randomTitle == 5){
-//     bookCover.innerHTML = "The " + nouns[Math.floor(Math.random()*nouns.length)] + " across the " + nouns[Math.floor(Math.random()*nouns.length)];
-// }
-
-// if (randomTitle == 6){
-//     bookCover.innerHTML = adjectives[Math.floor(Math.random()*adjectives.length)] + " and " + adjectives[Math.floor(Math.random()*adjectives.length)];
-// }
-
-// if (randomTitle == 7){
-//     bookCover.innerHTML = "A " + nouns[Math.floor(Math.random()*nouns.length)] + " in the " + nouns[Math.floor(Math.random()*nouns.length)];
-// }
+//structures
 
 if (randomTitle == 0){
     titleText.innerHTML = "The " + nouns[Math.floor(Math.random()*nouns.length)] + " in the " + nouns[Math.floor(Math.random()*nouns.length)];
